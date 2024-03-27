ca3ea9b87911ea7e8ff76b926f5d0cbcaef89102
py2fun <developer@py2fun.com>
2024-03-27 06:39:19 +0000

    add branch master

---
+++ first_project/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from first_app import views
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('home/',views.home,name="home"),
+    path('pytip/',views.pytip,name="educative"),
+]
