2cbd80e2e9d235b464ef3ec803745b508f06b071
py2fun <developer@py2fun.com>
2024-03-27 07:28:20 +0000

    add branch basic_form_validation

--- first_project/first_app/forms.py
+++ first_project/first_app/forms.py
@@ -5,3 +5,9 @@ class TestForm(forms.Form):
     boolean = forms.BooleanField()
     integer = forms.IntegerField()
     email = forms.EmailField()
+
+    def clean_integer(self):
+        integer= self.cleaned_data.get("integer")
+        if integer < 10:
+            raise forms.ValidationError("The integer must be greater than 10")
+        return integer
